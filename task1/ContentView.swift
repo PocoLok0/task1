d8abc6d8560845310ee547687e4f42ddebaa24dd
PocoLok0 <mega.emor22@gmail.com>
2021-03-21 16:18:21 +0200

    Initial Commit

---
+++ task1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  task1
+//
+//  Created by admin2 on 21.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
